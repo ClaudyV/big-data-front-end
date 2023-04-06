1869624c4b5f89da93754bc77faaf9e66c5789b8
Claude Vernet Michel <claudevernetmichel22@gmail.com>
2023-04-06 18:00:37 +0800

    [feat][Taiwan Section UI completed]

---
+++ src/components/Home/HomeLeftSection/HomeLeftSection.tsx
@@ -0,0 +1,13 @@
+import "./HomeLeftSection.css";
+
+const HomeLeftSection = () => {
+  return (
+    <div className="taiwan-wrapper">
+      <div className="taiwan">
+        <span>TAIWAN</span>
+      </div>
+    </div>
+  );
+};
+
+export default HomeLeftSection;
